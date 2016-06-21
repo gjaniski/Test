3107d0f91e7170f25bfa237810a5acc695591f80
Gerson <gjaniski@gmail.com>
2016-06-21 12:00:05 -0300

    Test

--- webapp/app/scripts/controllers/people.js
+++ webapp/app/scripts/controllers/people.js
@@ -1,9 +1,7 @@
 
-angular.module('webapp').controller('PeopleCtrl', ['$scope', 'PeopleService', '$window', function($scope, PeopleService, $window) {		
+angular.module('webapp').controller('PeopleCtrl', ['$scope', 'PeopleService', function($scope, PeopleService) {		
 	
 	$scope.people = [{}];
-	$scope.screenWidth = null;
-	$scope.screenHeigth = null;
 
 	$scope.getPeople = function(){
 		PeopleService.list().then(function(data){
@@ -12,35 +10,6 @@ angular.module('webapp').controller('PeopleCtrl', ['$scope', 'PeopleService', '$
 		});
 	};
 
-	angular.element($window).bind('resize', function () {
-	    //console.log("Width : ", $window.innerWidth);
-	    verifyWidth();
-	    $scope.$apply();
-	});
-
-	angular.element($window).bind('resize', function () {
-	    //console.log("Heigth : ", $window.innerHeight);
-	    verifyHeigth();
-	    $scope.$apply();
-	});
-
-	function verifyWidth(){
-	    if($window.innerWidth >= 800)
-	    	$scope.screenWidth = true;	    
-	    else
-	    	$scope.screenWidth = false;
-	};	
-
-	function verifyHeigth(){
-	    if($window.innerHeight >= 480)
-	    	$scope.screenHeigth = true;	    
-	    else
-	    	$scope.screenHeigth = false;
-	};
-
 	$scope.getPeople();
-
-	verifyWidth();
-	verifyHeigth();
 
 } ]);
